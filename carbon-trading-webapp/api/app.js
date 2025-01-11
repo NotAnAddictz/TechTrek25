74af40b3c9aa60ad6db9cf529d4f02286d35f6f7
pvsantoso <psantos@ampol.com.sg>
2025-01-11 14:34:02 +0800

    chore: clean app.js

--- carbon-trading-webapp/api/app.js
+++ carbon-trading-webapp/api/app.js
@@ -33,49 +33,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-// import express from 'express';
-// import { createClient } from '@supabase/supabase-js'
-
-// const companyRoutes = require('/routes/companyRoute.js')
-// const outstandingRoutes = require('/routes/outstandingRoute.js')
-
-// // const express = require('express')
-// const app = express();
-// const port = 3000;
-
-// const supabaseUrl = 'https://vfubkzgqdyqnrcumldqd.supabase.co'
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmdWJremdxZHlxbnJjdW1sZHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY1NjAzMjYsImV4cCI6MjA1MjEzNjMyNn0.gi0K8szcv58eqeHG5APKl51FSF0RUc28fzZzX_snH0Y'
-// const supabase = createClient(supabaseUrl, supabaseKey)
-
-// app.use(express.json())
-
-// // Routes
-// app.use('/api/company', companyRoutes); // Endpoints related to companies
-// app.use('/api/request', outstandingRoutes); // Endpoints related to requests
-  
-// app.listen(port, () => {
-// console.log(`Example app listening on port ${port}`)
-// })
-      
-// app.get('/outstandingRequests/:id', async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//         const {data, error} = await supabase
-//             .from('outstandingrequests')
-//             .select()
-//             .eq('companyid', id)
-//             .single();
-//         res.send(data);
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// });
-
-// // // Start the server
-// // app.listen(PORT, () => {
-// //     console.log(`Server is running on http://localhost:${PORT}`);
-// //   });
   
 
 
