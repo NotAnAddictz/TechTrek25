b94336ba2f2108b9e3f4720c00ceb335449638f8
sprintaway <ngyanjia26a2000@gmail.com>
2025-01-11 12:13:04 +0800

    implement popup message for overdue requests (7 days after request date)

--- carbon-trading-webapp/src/pages/OtherRequestPage.jsx
+++ carbon-trading-webapp/src/pages/OtherRequestPage.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css';
 
 const data = [
     {requestdate: "1/11/2015", companyname: "UOB", carbonprice: "10 SGD/Tonnes", carbonquantity: "20 Tonnes", 
@@ -13,16 +15,27 @@ const data = [
 
 const OtherRequestPage = () => {
     const [overdueRequests, setOverdueRequests] = React.useState([])
+    const [hasModal, setHasModal] = React.useState(false)
 
     useEffect(() => {
         const date = new Date()
         const lastWeek = new Date()
         lastWeek.setDate(date.getDate() - 7)
         const overdue = data.filter((val) => lastWeek > new Date(val.requestdate));
-        console.log(overdue)
         setOverdueRequests(overdue);
     }, [])
 
+    // console.log(overdueRequests.length)
+
+    useEffect(() => {
+        if (overdueRequests.length > 0) {
+            setHasModal(true)
+        }
+    }, [overdueRequests])
+
+    const closeModal = (e) => {
+        setHasModal(false)
+    }
 
   return (
 
@@ -58,8 +71,50 @@ const OtherRequestPage = () => {
         <button style={{backgroundColor: "red", color: "white", borderRadius: "5px"}}>
             Reject
         </button>
+        {hasModal && (
+        <div style={styles.overlay}>
+          <div style={styles.popup}>
+            <h2>Overdue Requests</h2>
+            <p>There are overdue requests!</p>
+            {<button onClick={closeModal} style={styles.button}>Close</button>}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
+
+const styles = {
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      zIndex: 1000,
+    },
+    popup: {
+      background: "white",
+      padding: "20px",
+      borderRadius: "10px",
+      boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+      textAlign: "center",
+      maxWidth: "400px",
+      width: "80%",
+    },
+    button: {
+      backgroundColor: "#007BFF",
+      color: "white",
+      border: "none",
+      padding: "10px 20px",
+      borderRadius: "5px",
+      cursor: "pointer",
+      marginTop: "10px",
+    }
+  };
 
 export default OtherRequestPage
