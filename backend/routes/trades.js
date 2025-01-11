91d9e985150803e1904bdf912f448845b934c82a
Kway Yi Shen <kwayyishen@gmail.com>
2025-01-11 13:06:31 +0800

    Updated trades to include companyname

--- backend/routes/trades.js
+++ backend/routes/trades.js
@@ -7,12 +7,16 @@ const router = express.Router(); // Create a router instance
 const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_API_KEY);
 router.get('/', async (req, res) => {
   try {
-    // Get all trades which are approved
+
     const { data, error } = await supabase
       .from('outstandingrequest')
-      .select('carbonunitprice,carbonquantity,companyid,requesttype,requeststatus')
-    res.send(data)
-    // Return all trades
+      .select('carbonunitprice,carbonquantity,companyid,requesttype,requeststatus, companyaccount!fk_company(companyname)')
+    if (data.length  > 0){
+        res.send(data)
+    }else{
+        res.status(404).json({ error: 'No trades found' });
+    }
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal server error' });
